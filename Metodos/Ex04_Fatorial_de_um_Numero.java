60c8fbaa99fe1a874c104c2962da9378728c691d
Victor Melo Homem <91573615+SkylineProgrammer@users.noreply.github.com>
2023-11-29 19:35:55 -0300

    Add files via upload

---
+++ Metodos/Ex04_Fatorial_de_um_Numero.java
@@ -0,0 +1,26 @@
+package lista_de_Exercicios_com_Metodos_na_Linguagem_de_Programacao_Java;
+import java.util.Scanner;
+
+public class Ex04_Fatorial_de_um_Numero {
+
+	public static int fatorial(int numero) {
+		
+		int multiplicacao = 1;
+		for (int i = 1; i <= numero; i++) {
+			multiplicacao *= i;
+		}
+		return multiplicacao;
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Digite um número: ");
+		int numero = scanner.nextInt();
+		
+		int resultado = fatorial(numero);
+		System.out.println("O fatorial de " + numero + " é " + resultado);
+
+		scanner.close();
+	}
+}
---
+++ Metodos/Ex07_Conversao_de_Temperatura.java
@@ -0,0 +1,21 @@
+package lista_de_Exercicios_com_Metodos_na_Linguagem_de_Programacao_Java;
+import java.util.Scanner;
+
+public class Ex07_Conversao_de_Temperatura {
+
+    public static double converterCelsiusParaFahrenheit(double celsius) {
+    	return (9.0 / 5.0) * celsius + 32;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite uma temperatura em graus Celsius: ");
+        double grausCelsius = scanner.nextDouble();
+
+        double grausFahrenheit = converterCelsiusParaFahrenheit(grausCelsius);
+        System.out.println("Temperatura equivalente em Fahrenheit: " + grausFahrenheit);
+
+        scanner.close();
+    }
+}
